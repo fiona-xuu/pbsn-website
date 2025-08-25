3ca8222406b38b977b67c47cf6e4294f52d8e007
fiona-xuu <fionaxu.99@gmail.com>
2025-08-25 16:44:37 -0400

    images

--- src/components/index page/Sponsors.tsx
+++ src/components/index page/Sponsors.tsx
@@ -1,35 +1,37 @@
+import sponsorsImage from "../../assets/sponsors.png";
+
 const Sponsors = () => {
     return (
         <section className="w-full bg-gray-100 flex items-center justify-center overflow-hidden">
             <div className="w-full max-w-7xl flex items-center justify-center">
                 <div className="flex animate-scroll">
                     <img
-                        src="/src/assets/sponsors.png"
+                        src={sponsorsImage}
                         alt="Past sponsors logos"
                         className="max-w-none h-32 lg:h-48 mr-8"
                     />
                     <img
-                        src="/src/assets/sponsors.png"
+                        src={sponsorsImage}
                         alt="Past sponsors logos"
                         className="max-w-none h-32 lg:h-48 mr-8"
                     />
                     <img
-                        src="/src/assets/sponsors.png"
+                        src={sponsorsImage}
                         alt="Past sponsors logos"
                         className="max-w-none h-32 lg:h-48 mr-8"
                     />
                     <img
-                        src="/src/assets/sponsors.png"
+                        src={sponsorsImage}
                         alt="Past sponsors logos"
                         className="max-w-none h-32 lg:h-48 mr-8"
                     />
                     <img
-                        src="/src/assets/sponsors.png"
+                        src={sponsorsImage}
                         alt="Past sponsors logos"
                         className="max-w-none h-32 lg:h-48 mr-8"
                     />
                     <img
-                        src="/src/assets/sponsors.png"
+                        src={sponsorsImage}
                         alt="Past sponsors logos"
                         className="max-w-none h-32 lg:h-48"
                     />
